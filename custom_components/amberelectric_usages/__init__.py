338cf80481de6c8a97a14bedde0796996ad30ae5
Bao Truong <tmbao.1995@gmail.com>
2024-12-13 22:21:56 +0700

    Fix config flow for amberapi v2

--- custom_components/amberelectric_usages/__init__.py
+++ custom_components/amberelectric_usages/__init__.py
@@ -1,36 +1,33 @@
 """Support for Amber Electric - Usages."""
 
-from amberelectric import Configuration, ApiClient
-from amberelectric.api import amber_api
+import amberelectric
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_TOKEN
 from homeassistant.core import HomeAssistant
 
-from .const import CONF_SITE_ID, DOMAIN, PLATFORMS
+from .const import CONF_SITE_ID, PLATFORMS
 from .coordinator import AmberUsagesCoordinator
 
+type AmberConfigEntry = ConfigEntry[AmberUsagesCoordinator]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+
+async def async_setup_entry(hass: HomeAssistant, entry: AmberConfigEntry) -> bool:
     """Set up Amber Electric from a config entry."""
-    configuration = Configuration(access_token=entry.data[CONF_API_TOKEN])
-    api_client = ApiClient(configuration)
-    api_instance = amber_api.AmberApi(api_client)
+    configuration = amberelectric.Configuration(access_token=entry.data[CONF_API_TOKEN])
+    api_client = amberelectric.ApiClient(configuration)
+    api_instance = amberelectric.AmberApi(api_client)
     site_id = entry.data[CONF_SITE_ID]
 
     usages_coordinator = AmberUsagesCoordinator(
         hass, api_instance, site_id, entry.title
     )
     await usages_coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = usages_coordinator
+    entry.runtime_data = usages_coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: AmberConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
